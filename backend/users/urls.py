c55e0336d99ef01a7bdee9fd619fa5dd5be88134
pujam-dev <puja.mahapatra@argusoft.com>
2025-08-29 09:09:39 +0530

    Delete backend directory

--- backend/users/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path,include
-from . import views
-urlpatterns = [
-    path('register/',views.UserRegistrationView.as_view(),name='register'),
-    path('login/',views.UserLoginView.as_view(),name='login'),
-    path('profile/',views.UserProfileView.as_view(),name='profile'),
-    path('logout/',views.LogoutView.as_view(),name='logout'),
-]
